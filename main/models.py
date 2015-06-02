634efafc9201c6fc27b62ef0778333e6f9365b8c
Coding Campus <cc@codingcamp.us>
2015-06-02 09:20:14 -0600

    address model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from main_models.item import *
+from main_models.address import *
 from main_models.order import *
 from main_models.tag import *
 from main_models.user import ShopUser
