be352a59cc55155980f1687ea7b60b10270d8194
Coding Campus <cc@codingcamp.us>
2015-06-02 15:59:24 -0600

    list view

---
+++ main/main_views/item_list.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from rest_framework import mixins
+from main.models import Item
+from main.serializers import TaggedModelSerializer
+
+
+class ItemList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Item.objects.all()
+    serializer_class = TaggedModelSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        return self.create(request, *args, **kwargs)
