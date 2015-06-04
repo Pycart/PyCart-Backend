ddfbfae48f4276b35a9d3836911e2ca9c4d9556f
jaredmackey <mackeyja92@gmail.com>
2015-06-03 18:24:21 -0600

    built shop user serializer.

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
+from main.main_models.user import ShopUser
 from main_models.item import Item
 from main.main_models.order import Order, Status
+
+
+class ShopUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShopUser
+        fields = ('email', 'first_name', 'last_name')
+        read_only_fields = ('email', )
 
 
 class ItemSerializer(TaggitSerializer, serializers.ModelSerializer):
