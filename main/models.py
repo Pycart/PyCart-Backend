3f0ea7ae23e940f600c3bc97e0602e545b81ee21
jaredmackey <mackeyja92@gmail.com>
2015-06-03 19:11:21 -0600

    Moved import from init to models.py

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
+from django.db import models
+from main import signals
 from main_models.item import *
 from main_models.order import *
 from main_models.tag import *
 from main_models.user import ShopUser
-
 
 
 # Create your models here.
