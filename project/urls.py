7f798d4ba45b1900352c51f32cef3ccc4d0a71c6
Alex Lyman <alexjohnlyman@gmail.com>
2015-06-17 16:08:11 -0600

    Completed updating urls

--- project/urls.py
+++ project/urls.py
@@ -24,11 +24,9 @@ from main.main_views.order import OrdersView, RecentOrdersView, AddItemToOrderVi
 from main.main_views.tags import TagList, HeaderTagList, SubheaderTagList
 from main.main_views.admin_dashboard import *
 from main.main_views.user_account import UserView
-from main.main_views.user_list import ShopUserList
 from main.main_views.save_card import CardDetail
 from main.main_views.user_address import *
 from main.main_views.stock_record_views import StockRecords
-from main.main_views.save_card import CardList
 from main.main_views.save_card import CardList
 
 urlpatterns = [
@@ -38,26 +36,13 @@ urlpatterns = [
     url(r'auth/me/', UserView.as_view(), name='user_details'),
     url(r'auth/', include('djoser.urls')),
 
-    # TODO: Remove these urls
-    url(r'^items_list/$', ItemList.as_view(), name="items_list"),
-    url(r'^items_detail/(?P<pk>[0-9]+)/$', ItemDetail.as_view(), name='item_detail'),
-    url(r'^items_search/$', ItemSearch.as_view(), name="items_search"),
+    url(r'^items/$', ItemList.as_view(), name="items_list"),
+    url(r'^items/(?P<pk>[0-9]+)/$', ItemDetail.as_view(), name='item_detail'),
+    url(r'^items/search/$', ItemSearch.as_view(), name="items_search"),
 
-    # TODO: Change front end to go to these urls instead
-    # url(r'^items/$', ItemList.as_view(), name="items_list"),
-    # url(r'^items/(?P<pk>[0-9]+)/$', ItemDetail.as_view(), name='item_detail'),
-    # url(r'^items/search/$', ItemSearch.as_view(), name="items_search"),
-
-
-    # TODO: Remove these urls
-    url(r'^tags_list/$', TagList.as_view(), name="tags_list"),
-    url(r'^header_tags/$', HeaderTagList.as_view(), name="header_tags"),
-    url(r'^subheader_tags/$', SubheaderTagList.as_view(), name="subheader_tags"),
-
-    # TODO: Change front end to go to these urls instead
-    # url(r'^tags/$', TagList.as_view(), name="tags_list"),
-    # url(r'^tags/headers/$', HeaderTagList.as_view(), name="header_tags"),
-    # url(r'^tags/subheaders/$', SubheaderTagList.as_view(), name="subheader_tags"),
+    url(r'^tags/$', TagList.as_view(), name="tags_list"),
+    url(r'^tags/headers/$', HeaderTagList.as_view(), name="header_tags"),
+    url(r'^tags/subheaders/$', SubheaderTagList.as_view(), name="subheader_tags"),
 
     url(r'^admin_dashboard/items/$', AdminListCreateItem.as_view(), name="create_item"),
     url(r'^admin_dashboard/items/(?P<pk>[0-9]+)/$', AdminItemDetail.as_view(), name="item_detail_update"),
@@ -65,30 +50,15 @@ urlpatterns = [
     url(r'^admin_dashboard/options/(?P<pk>[0-9]+)/$', AdminOptionDetail.as_view(), name="option_detail_update"),
     url(r'^admin_dashboard/status/$', AdminListCreateStatus.as_view(), name="create_status"),
     url(r'^admin_dashboard/status/(?P<pk>[0-9]+)/$', AdminStatusDetail.as_view(), name="status_detail_update"),
+    url(r'^admin_dashboard/shop_user_list/', AdminShopUserList.as_view(), name="user_list"),
 
-    # TODO: Remove these urls
-    url(r'^add_to_cart/$', AddItemToOrderView.as_view(), name="add_to_cart"),
-    url(r'^get_cart/$', GetCart.as_view(), name="get_cart"),
+    url(r'^cart/$', GetCart.as_view(), name="get_cart"),
+    url(r'^cart/add/$', AddItemToOrderView.as_view(), name="add_to_cart"),
 
-    # TODO: Change front end to go to these urls instead
-    # url(r'^cart/$', GetCart.as_view(), name="get_cart"),
-    # url(r'^cart/add/$', AddItemToOrderView.as_view(), name="add_to_cart"),
-
-
-    # TODO: Remove these urls
-    url(r'^user_dashboard/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name="user_dashboard"),
-    url(r'^allOrdersList/', OrdersView.as_view(), name="all_orders"),
-    url(r'^recentOrdersList/', RecentOrdersView.as_view(), name="recent_orders"),
-    url(r'^order_detail/(?P<pk>[0-9]+)/$', OrderDetail.as_view(), name='order_detail'),
-    url(r'^ShopUserList/', ShopUserList.as_view(), name="user_list"),
-
-
-    # TODO: Change front end to go to these urls instead
-    # url(r'^user/user_dashboard/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name="user_dashboard"),
-    # url(r'^user/orders/$', OrdersView.as_view(), name="all_orders"),
-    # url(r'^user/orders/recent/$', RecentOrdersView.as_view(), name="recent_orders"),
-    # url(r'^user/orders/(?P<pk>[0-9]+)/$', OrderDetail.as_view(), name='order_detail'),
-
+    url(r'^user/user_dashboard/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name="user_dashboard"),
+    url(r'^user/orders/$', OrdersView.as_view(), name="all_orders"),
+    url(r'^user/orders/recent/$', RecentOrdersView.as_view(), name="recent_orders"),
+    url(r'^user/orders/(?P<pk>[0-9]+)/$', OrderDetail.as_view(), name='order_detail'),
 
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
